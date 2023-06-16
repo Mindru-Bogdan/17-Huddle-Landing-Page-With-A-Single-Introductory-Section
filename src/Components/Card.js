b9480f5530690713e4e0560e68648bf0886a93d1
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-16 17:56:56 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import BackgroundDesktop from '../../src/bg-desktop.svg';
+import BackgroundMobile from '../../src/bg-mobile.svg';
+import Logo from '../../src/logo.svg';
+import Illustration from '../../src/illustration-mockups.svg';
+
 import classes from './Card.module.css';
 
 const Card = () => {
   return (
-    <div>
+    <div className={classes.card}>
+      <img src={BackgroundDesktop} className={classes.bgDesktop} />
+      <img src={BackgroundMobile} className={classes.bgMobile} />
+      <img src={Logo} className={classes.logo} />
+      <img src={Illustration} className={classes.illustration} />
       <h1>Build The Community Your Fans Will Love</h1>
-      <p>
+      <p className={classes.description}>
         Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with
         your users as you engage in genuine discussion.{' '}
       </p>
-      <p>Register</p>
+      <p className={classes.register}>Register</p>
+      <FontAwesomeIcon icon={faFacebookF} className={classes.iconFacebook} />
+      <FontAwesomeIcon icon={faTwitter} className={classes.iconTwitter} />
+      <FontAwesomeIcon icon={faInstagram} className={classes.iconInstagram} />
     </div>
   );
 };
