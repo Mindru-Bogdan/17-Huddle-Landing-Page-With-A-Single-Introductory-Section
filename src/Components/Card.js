4b33ae5fdcc59c78d660097767e90e576d2f0545
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-16 13:47:49 +0300

    Post initial files

---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import classes from './Card.module.css';
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Build The Community Your Fans Will Love</h1>
+      <p>
+        Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with
+        your users as you engage in genuine discussion.{' '}
+      </p>
+      <p>Register</p>
+    </div>
+  );
+};
+
+export default Card;
